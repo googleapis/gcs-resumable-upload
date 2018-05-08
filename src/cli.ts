84f98fa0f9bea67bf491511c9256c9991f547b8d
Justin Beckwith <justin.beckwith@gmail.com>
2018-05-08 09:14:12 -0700

    chore: re-work some of the tests

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-const upload = require('./');
+
+import {upload, RequestResponse} from '.';
+
 const args = process.argv.slice(2);
 const opts = {
   bucket: args[0],
   file: args[1]
 };
-import * as r from 'request';
 
 process.stdin.pipe(upload(opts))
     .on('error', console.error)
-    .on('response', (resp: r.Response, metadata: {mediaLink: string}) => {
+    .on('response', (resp: RequestResponse, metadata: {mediaLink: string}) => {
       if (!metadata || !metadata.mediaLink) return;
       console.log('uploaded!');
       console.log(metadata.mediaLink);
