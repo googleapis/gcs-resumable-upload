0b6f9ed8187978f54b36d6e9b240e27d45f39f7e
Justin Beckwith <justin.beckwith@gmail.com>
2018-12-05 13:00:23 -0800

    chore: update synth and common config (#150)

--- synth.py
+++ synth.py
@@ -5,6 +5,5 @@ import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(package_name="gcs-resumable-upload")
-s.copy(templates)
-
+templates = common_templates.node_library()
+s.copy(templates, excludes=["LICENSE"])
