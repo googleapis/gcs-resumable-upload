a741efd4ed35ab274e9bdaa451bfa24604cd0bf6
Justin Beckwith <justin.beckwith@gmail.com>
2018-12-12 08:39:34 -0800

    fix: fix the unit tests (#161)

--- test/test.ts
+++ test/test.ts
@@ -884,7 +884,7 @@ describe('gcs-resumable-upload', () => {
       const scope = nock(REQ_OPTS.url).get(queryPath).reply(200);
       up.getRequestStream(
           REQ_OPTS, (requestStream: stream.Readable&{callback: Function}) => {
-            assert.strictEqual(requestStream.callback.toString(), '() => { }');
+            assert.strictEqual(typeof requestStream.callback, 'function');
             done();
           });
     });
