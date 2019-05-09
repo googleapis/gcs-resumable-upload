b39579ac5ec6002d36890204d228c4c7c0fb5cc4
Benjamin E. Coe <bencoe@google.com>
2019-05-09 12:19:00 -0700

    build: upgrade gts and other dependencies for 2.x release (#220)

--- system-test/kitchen.ts
+++ system-test/kitchen.ts
@@ -13,27 +13,28 @@ import {createURI, upload} from '../src';
 const bucketName = process.env.BUCKET_NAME || 'gcs-resumable-upload-test';
 
 describe('end to end', () => {
-  it('should work', (done) => {
+  it('should work', done => {
     let uploadSucceeded = false;
     fs.createReadStream('daw.jpg')
-        .on('error', done)
-        .pipe(upload({
+      .on('error', done)
+      .pipe(
+        upload({
           bucket: bucketName,
           file: 'daw.jpg',
-          metadata: {contentType: 'image/jpg'}
-        }))
-        .on('error', done)
-        .on('response',
-            (resp) => {
-              uploadSucceeded = resp.status === 200;
-            })
-        .on('finish', () => {
-          assert.strictEqual(uploadSucceeded, true);
-          done();
-        });
+          metadata: {contentType: 'image/jpg'},
+        })
+      )
+      .on('error', done)
+      .on('response', resp => {
+        uploadSucceeded = resp.status === 200;
+      })
+      .on('finish', () => {
+        assert.strictEqual(uploadSucceeded, true);
+        done();
+      });
   });
 
-  it('should resume an interrupted upload', (done) => {
+  it('should resume an interrupted upload', done => {
     fs.stat('daw.jpg', (err, fd) => {
       assert.ifError(err);
 
@@ -41,55 +42,56 @@ describe('end to end', () => {
 
       // tslint:disable-next-line no-any
       type DoUploadCallback = (...args: any[]) => void;
-      const doUpload =
-          (opts: {interrupt?: boolean}, callback: DoUploadCallback) => {
-            let sizeStreamed = 0;
-            let destroyed = false;
+      const doUpload = (
+        opts: {interrupt?: boolean},
+        callback: DoUploadCallback
+      ) => {
+        let sizeStreamed = 0;
+        let destroyed = false;
 
-            const ws = upload({
-              bucket: bucketName,
-              file: 'daw.jpg',
-              metadata: {contentType: 'image/jpg'}
-            });
+        const ws = upload({
+          bucket: bucketName,
+          file: 'daw.jpg',
+          metadata: {contentType: 'image/jpg'},
+        });
 
-            fs.createReadStream('daw.jpg')
-                .on('error', callback)
-                .on('data',
-                    function(this: Readable, chunk) {
-                      sizeStreamed += chunk.length;
+        fs.createReadStream('daw.jpg')
+          .on('error', callback)
+          .on('data', function(this: Readable, chunk) {
+            sizeStreamed += chunk.length;
 
-                      if (!destroyed && opts.interrupt &&
-                          sizeStreamed >= size / 2) {
-                        // stop sending data half way through
-                        destroyed = true;
-                        this.destroy();
-                        ws.destroy(new Error('Interrupted'));
-                      }
-                    })
-                .pipe(ws)
-                .on('error', callback)
-                .on('metadata', callback.bind(null, null));
-          };
+            if (!destroyed && opts.interrupt && sizeStreamed >= size / 2) {
+              // stop sending data half way through
+              destroyed = true;
+              this.destroy();
+              ws.destroy(new Error('Interrupted'));
+            }
+          })
+          .pipe(ws)
+          .on('error', callback)
+          .on('metadata', callback.bind(null, null));
+      };
 
       doUpload({interrupt: true}, (err: Error) => {
         assert.strictEqual(err.message, 'Interrupted');
 
         doUpload({interrupt: false}, (err: Error, metadata: {size: number}) => {
           assert.ifError(err);
-          assert.equal(metadata.size, size);
+          assert.strictEqual(Number(metadata.size), size);
           done();
         });
       });
     });
   });
 
-  it('should just make an upload URI', (done) => {
+  it('should just make an upload URI', done => {
     createURI(
-        {
-          bucket: bucketName,
-          file: 'daw.jpg',
-          metadata: {contentType: 'image/jpg'}
-        },
-        done);
+      {
+        bucket: bucketName,
+        file: 'daw.jpg',
+        metadata: {contentType: 'image/jpg'},
+      },
+      done
+    );
   });
 });
