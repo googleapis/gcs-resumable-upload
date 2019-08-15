f78dfc3c1db658ff6eccd195ebde999bd20c9ca7
Stephen <stephenplusplus@users.noreply.github.com>
2019-08-15 10:31:04 -0400

    this here too

--- .jsdoc.js
+++ .jsdoc.js
@@ -36,7 +36,7 @@ module.exports = {
     includePattern: '\\.js$'
   },
   templates: {
-    copyright: 'Copyright 2018 Google, LLC.',
+    copyright: 'Copyright 2018 Google LLC',
     includeDate: false,
     sourceFiles: false,
     systemName: 'gcs-resumable-upload',
